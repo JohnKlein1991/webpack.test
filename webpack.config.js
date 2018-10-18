b17aacd7a6d92d1552e4973b19f217d1b99fda4a
Andrew Marhai <buk2018irinam@gmail.com>
2018-10-18 14:26:12 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+          test: /\.css$/,
+          use: [
+            "style-loader",
+            "css-loader"
+          ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      header: '<h1>Title of the page</h1>',
+      main: '<div class="main">MAIN information</div>',
+      footer: '<p>Bottom of the page</p>',
+      // Load a custom template (lodash by default see the FAQ for details)
+      template: 'index.html'
+    })
+  ]
+};
